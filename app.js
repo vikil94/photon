db9e1f199a93861756b93d701c546165454c2602
Vikil Naik <vikilnaik@Vikils-MacBook-Pro.local>
2021-01-11 15:13:50 -0500

    Simplified the code by creating functions for repeated code

--- app.js
+++ app.js
@@ -15,18 +15,19 @@ function updateInput(e) {
 	searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-	const dataFetch = await fetch(
-		"https://api.pexels.com/v1/curated?per_page=15&page=1",
-		{
-			method: "GET",
-			headers: {
-				Accept: "application/json",
-				Authorization: auth,
-			},
-		}
-	);
+async function fetchAPI(url) {
+	const dataFetch = await fetch(url, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			Authorization: auth,
+		},
+	});
 	const data = await dataFetch.json();
+	return data;
+}
+
+function generatePictures(data) {
 	data.photos.forEach((photo) => {
 		const galleryImg = document.createElement("div");
 		galleryImg.classList.add("gallery-img");
@@ -37,26 +38,18 @@ async function curatedPhotos() {
 	});
 }
 
-async function searchPhotos(query) {
-	const dataFetch = await fetch(
-		`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
-		{
-			method: "GET",
-			headers: {
-				Accept: "application/json",
-				Authorization: auth,
-			},
-		}
+async function curatedPhotos() {
+	const data = await fetchAPI(
+		"https://api.pexels.com/v1/curated?per_page=15&page=1"
 	);
-	const data = await dataFetch.json();
-	data.photos.forEach((photo) => {
-		const galleryImg = document.createElement("div");
-		galleryImg.classList.add("gallery-img");
-		galleryImg.innerHTML = `<img src=${photo.src.large}> </img> 
-        <p>${photo.photographer}</p>
-        `;
-		gallery.appendChild(galleryImg);
-	});
+	generatePictures(data);
+}
+
+async function searchPhotos(query) {
+	const data = await fetchAPI(
+		`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+	);
+	generatePictures(data);
 }
 
 curatedPhotos();
